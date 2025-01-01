88a475efa4be54e3086e25e97b591e5075b9b58d
kdu228 <40692848+kdu228@users.noreply.github.com>
2025-01-01 18:44:54 +0900

    Update sample.py

--- pycodes/sample.py
+++ pycodes/sample.py
@@ -4,3 +4,11 @@ Created on Wed Jan  1 12:36:29 2025
 
 @author: kdu22
 """
+from datetime import datetime
+import tzlocal # $ pip install tzlocal
+
+now = datetime.now(tzlocal.get_localzone())
+print(now.strftime('%Z'))
+# -> MSK
+print(now.tzname())
+# -> MSK
