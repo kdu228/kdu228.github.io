a687e6ec35e5037eaaa10ee5e4403459b604506a
kdu228 <40692848+kdu228@users.noreply.github.com>
2025-01-01 19:38:20 +0900

    Update sample.py

--- pycodes/sample.py
+++ pycodes/sample.py
@@ -4,11 +4,14 @@ Created on Wed Jan  1 12:36:29 2025
 
 @author: kdu22
 """
-from datetime import datetime
-import tzlocal # $ pip install tzlocal
-
-now = datetime.now(tzlocal.get_localzone())
-print(now.strftime('%Z'))
-# -> MSK
-print(now.tzname())
-# -> MSK
+import pytz
+import datetime
+# Get the current time in UTC
+utc_now = datetime.datetime.utcnow()
+# Get the local timezone
+local_tz = pytz.timezone('Asia/Kolkata')
+# Convert the UTC time to local time
+local_now = utc_now.replace(tzinfo=pytz.utc).astimezone(local_tz)
+# Print the local time and timezone
+print("Local time: ", local_now.strftime('%Y-%m-%d %H:%M:%S'))
+print("Timezone: ", local_now.tzinfo)
